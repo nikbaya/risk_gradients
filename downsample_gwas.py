a1bbbabf7e0953841ddfac3da18e26a865e08126
nikbaya <nbaya@broadinstitute.org>
2019-06-06 14:19:36 -0400

    updated downsample function

--- downsample_gwas.py
+++ downsample_gwas.py
@@ -12,16 +12,21 @@ Also options for varying case/control ratios
 
 import hail as hl
 import argparse
+import numpy as np
+from hail.utils.java import Env
+
 
 parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument('--phen', type=str, required=True, help="phenotype code (e.g. for height, phen = 50")
-parser.add_argument('--variant_set', type=str, required=True, help="set of variants to use")
+parser.add_argument('--frac_all_ls', nargs='+', type=float, required=False, default=None, help="downsampling fraction of all individuals")
+parser.add_argument('--frac_cas_ls', nargs='+', type=float, required=False, default=None, help="downsampling fraction of cases")
+parser.add_argument('--frac_con_ls', nargs='+', type=float, required=False, default=None, help="downsampling fraction of controls")
+args = parser.parse_args()
 
 phen = args.phen
-    desc = phen_dict[phen]
-    batch = args.batch
-    n_chunks = args.n_chunks #number of subgroups (or "chunks")
-    variant_set = str(args.variant_set)
+frac_all_ls = args.frac_all_ls
+frac_cas_ls = args.frac_cas_ls
+frac_con_ls = args.frac_con_ls
 
 variant_set = 'hm3'
 phen_dict = {
@@ -29,29 +34,136 @@ phen_dict = {
     '2443':'diabetes',
     '21001':'bmi',
 }
+gc_bucket = 'gs://nbaya/risk_gradients/gwas/'
 
-mt0 = hl.read_matrix_table(f'gs://nbaya/split/ukb31063.{variant_set}_variants.gwas_samples_repart.mt')
-
-print('\nReading UKB phenotype...')
-#        mt0 = hl.read_matrix_table('gs://nbaya/split/ukb31063.hm3_variants.gwas_samples_v2.mt') #old version
+def get_mt(phen, variant_set):
+    mt0 = hl.read_matrix_table(f'gs://nbaya/split/ukb31063.{variant_set}_variants.gwas_samples_repart.mt')
     
-phen_tb0 = hl.import_table('gs://phenotype_31063/ukb31063.phesant_phenotypes.both_sexes.tsv.bgz',
-                           missing='',impute=True,types={'"userId"': hl.tstr}).rename({ '"userId"': 's', '"'+phen+'"': 'phen'})
-phen_tb0 = phen_tb0.key_by('s')
+    print(f'\nReading UKB phenotype {phen_dict[phen]} (code: {phen})...')
+    #        mt0 = hl.read_matrix_table('gs://nbaya/split/ukb31063.hm3_variants.gwas_samples_v2.mt') #old version
+        
+    phen_tb0 = hl.import_table('gs://phenotype_31063/ukb31063.phesant_phenotypes.both_sexes.tsv.bgz',
+                               missing='',impute=True,types={'"userId"': hl.tstr}).rename({ '"userId"': 's', '"'+phen+'"': 'phen'})
+    phen_tb0 = phen_tb0.key_by('s')
+    phen_tb = phen_tb0.select(phen_tb0['phen'])    
+    
+    mt1 = mt0.annotate_cols(phen_str = hl.str(phen_tb[mt0.s]['phen']).replace('\"',''))
+    mt1 = mt1.filter_cols(mt1.phen_str == '',keep=False)
+    
+    if phen_tb.phen.dtype == hl.dtype('bool'):
+        mt1 = mt1.annotate_cols(phen = hl.bool(mt1.phen_str)).drop('phen_str')
+    else:
+        mt1 = mt1.annotate_cols(phen = hl.float64(mt1.phen_str)).drop('phen_str')            
+    
+    #Remove withdrawn samples
+    withdrawn = hl.import_table('gs://nbaya/w31063_20181016.csv',missing='',no_header=True)
+    withdrawn_set = set(withdrawn.f0.take(withdrawn.count()))
+    mt1 = mt1.filter_cols(hl.literal(withdrawn_set).contains(mt1['s']),keep=False) 
+    mt1 = mt1.key_cols_by('s')
+    
+    return mt1
+    
+def downsample(mt, frac, phen, for_cases=None, seed = None):
+    assert frac <= 1 and frac >= 0, "frac must be in [0,1]"
+    phen_name = phen._ir.name
+    n = mt.count_cols()
+    n_cas = mt.filter_cols(mt[phen_name]==1).count_cols()
+    print('************')
+    print('Downsampling '+('all' if for_cases is None else ('cases'*for_cases+'controls'*(for_cases==0))))
+    print(f'n: {n}\nn_cas: {n_cas} ({round(100*n_cas/n,3)}%)\nn_con: {n-n_cas} ({round(100*(1-n_cas/n),3)}%)')
+    print('************')
+    col_key = mt.col_key
+    seed = seed if seed is not None else int(str(Env.next_seed())[:8])
+    randstate = np.random.RandomState(int(seed)) #seed random state for replicability
+    for_cases = bool(for_cases) if for_cases != None else None
+    filter_arg = (mt[phen_name] == (for_cases==0)) if for_cases != None else (hl.is_defined(mt[phen_name])==False)
+    mtA = mt.filter_cols(filter_arg) #keep all individuals in this mt
+    mtB = mt.filter_cols(filter_arg , keep=False) #downsample individuals in this mt
+    mtB = mtB.add_col_index('col_idx_tmpB')
+    mtB = mtB.key_cols_by('col_idx_tmpB')
+    nB = n_cas*for_cases + (n-n_cas)*(for_cases==0) if for_cases is not None else n
+    n_keep = int(nB*frac)
+    labels = ['A']*(n_keep)+['B']*(nB-n_keep)
+    randstate.shuffle(labels)
+    mtB = mtB.annotate_cols(label = hl.literal(labels)[hl.int32(mtB.col_idx_tmpB)])
+    mtB = mtB.filter_cols(mtB.label == 'A')
+    mtB = mtB.key_cols_by(*col_key)
+    mtB = mtB.drop('col_idx_tmpB','label')
+    mt1 = mtA.union_cols(mtB) 
+    n_new = mt1.count_cols()
+    n_cas_new = mt1.filter_cols(mt1[phen_name]==1).count_cols()
+    print('************')
+    print(f'n: {n} -> {n_new}')
+    print(f'n_cas: {n_cas} -> {n_cas_new} ({round(100*n_cas_new/n_new,3)}%)')
+    print(f'n_con: {n-n_cas} -> {n_new-n_cas_new} ({round(100*(1-n_cas_new/n_new),3)}%)')
+    print('************')
+    return mt1, n_new, n_cas
+        
+frac = 0.5
+downsample_cases = 1
+mt = hl.methods.balding_nichols_model(1,100,100)
+mt = mt.annotate_cols(y = hl.rand_bool(0.2))
+phen = mt.y
+mt1 = downsample(mt=mt,frac=frac,phen=phen,for_cases=1)
 
-phen_tb = phen_tb0.select(phen_tb0['phen'])    
 
-mt1 = mt0.annotate_cols(phen_str = hl.str(phen_tb[mt0.s]['phen']).replace('\"',''))
-mt1 = mt1.filter_cols(mt1.phen_str == '',keep=False)
 
-if phen_tb.phen.dtype == hl.dtype('bool'):
-    mt1 = mt1.annotate_cols(phen = hl.bool(mt1.phen_str)).drop('phen_str')
-else:
-    mt1 = mt1.annotate_cols(phen = hl.float64(mt1.phen_str)).drop('phen_str')            
+if __name__ == "__main__":    
+    header =  '\n*************\n'
+    header += f'Phenotype: {phen_dict[phen]} (code: {phen})\n'    
+    header += f'Downsampling fractions for all: {frac_all_ls}\n' if frac_all_ls != None else ''
+    header += f'Downsampling fractions for cases: {frac_cas_ls}\n' if frac_cas_ls != None else ''
+    header += f'Downsampling fractions for controls: {frac_con_ls}\n' if frac_con_ls != None else ''
+    header += '*************'
+    print(header)
+    
+    frac_all_ls = [1] if frac_all_ls == None else frac_all_ls
+    frac_cas_ls = [1] if frac_cas_ls == None else frac_cas_ls
+    frac_con_ls = [1] if frac_con_ls == None else frac_con_ls
+    
+    mt = get_mt(phen, variant_set)
 
-#Remove withdrawn samples
-withdrawn = hl.import_table('gs://nbaya/w31063_20181016.csv',missing='',no_header=True)
-withdrawn_set = set(withdrawn.f0.take(withdrawn.count()))
-mt1 = mt1.filter_cols(hl.literal(withdrawn_set).contains(mt1['s']),keep=False) 
-mt1 = mt1.key_cols_by('s')
+    cov_list = [ mt['isFemale'], mt['age'], mt['age_squared'], mt['age_isFemale'],
+                    mt['age_squared_isFemale'] ]+ [mt['PC{:}'.format(i)] for i in range(1, 21)] 
+    
+    for frac_all in frac_all_ls: #for each downsampling fraction in frac_all_ls
+        mt1, _, _ = downsample(mt=mt,frac=frac_all,phen=mt.phen,for_cases=None)
+        for frac_cas in frac_cas_ls:
+            mt2, _, _ = downsample(mt=mt1,frac=frac_cas,phen=mt1.phen,for_cases=1)
+            for frac_con in frac_con_ls:
+                mt3, n, n_cas = downsample(mt=mt2,frac=frac_con,phen=mt2.phen,for_cases=0)
+                ht = hl.linear_regression_rows(
+                        y=mt.phen,
+                        x=mt.dosage,
+                        covariates=[1]+cov_list,
+                        pass_through = ['rsid'])
+            
+                ht = ht.rename({'rsid':'SNP'}).key_by('SNP')
 
+                ss_template = hl.read_table('gs://nbaya/rg_sex/hm3.sumstats_template.ht/')
+                ss_template  = ss_template .key_by('SNP')
+                
+                ss = ss_template.annotate(N = n)
+                ss = ss.annotate(Z = ht[ss.SNP]['Z'])
+                
+                if frac_con == 1 and frac_cas ==1:
+                    path = f'{gc_bucket}{phen}.downsampled.n_{n}.tsv.bgz' 
+                else:
+                    path = f'{gc_bucket}{phen}.downsampled.n_{n}.n_cas_{n_cas}.tsv.bgz' 
+                ss.export(path)
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        
