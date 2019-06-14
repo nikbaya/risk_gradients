17facde4f0d5ad2861d346a270052d3cc01b9274
Nik Baya <40802812+nikbaya@users.noreply.github.com>
2019-06-14 09:27:54 -0400

    updated to reconcile pull/push

--- downsample_gwas.py
+++ downsample_gwas.py
@@ -1,63 +1,51 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 11 11:39:46 2019
-
-Calculate PGS from summary statistics and genotypes, pruning to certain variants 
-
+Created on Wed Jun  5 11:41:38 2019
+Run downsampled GWAS on selected phenotypes
+Also options for varying case/control ratios
 @author: nbaya
 """
 
 import hail as hl
 import argparse
-import scipy.stats as stats
 import numpy as np
-import subprocess
-import pandas as pd
-import datetime as dt
-import os
 from hail.utils.java import Env
-
+import datetime as dt
+import subprocess
 
 
 parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument('--phen_ls', nargs='+', type=str, required=True, help="phenotype code (e.g. for height, phen = 50")
-#parser.add_argument('--n_ls', nargs='+', type=int, required=True, help="number of samples for each phenotype, ordered the same as phen_ls")
-#parser.add_argument('--n_cas_ls', nargs='+', type=int, required=False, default=None, help="number of cases for each phenotype, ordered the same as phen_ls")
 parser.add_argument('--frac_all_ls', nargs='+', type=float, required=False, default=None, help="downsampling fraction of all individuals")
 parser.add_argument('--frac_cas_ls', nargs='+', type=float, required=False, default=None, help="downsampling fraction of cases")
 parser.add_argument('--frac_con_ls', nargs='+', type=float, required=False, default=None, help="downsampling fraction of controls")
-parser.add_argument('--seed', type=int, required=False, default=None, help="random seed for replicability")
+parser.add_argument('--seed', type=int, required=False, default=1, help="random seed for replicability")
 args = parser.parse_args()
 
 phen_ls = args.phen_ls
-#n_ls = args.n_ls
-#n_cas_ls = args.n_cas_ls
-#n_cas_ls = n_ls if n_cas_ls == None else n_cas_ls
 frac_all_ls = args.frac_all_ls
 frac_cas_ls = args.frac_cas_ls
 frac_con_ls = args.frac_con_ls
 seed = args.seed
-seed = 1 if seed is None else seed
 
 frac_all_ls = [1] if frac_all_ls == None else frac_all_ls
 frac_cas_ls = [1] if frac_cas_ls == None else frac_cas_ls
 frac_con_ls = [1] if frac_con_ls == None else frac_con_ls
 
-
-wd = 'gs://nbaya/risk_gradients/'
-local_wd = '/home/nbaya/' #working directory for VM instance
-gwas_wd = wd+'gwas/'
+variant_set = 'hm3'
 phen_dict = {
     '50':'height',
     '2443':'diabetes',
     '21001':'bmi',
 }
+gwas_wd = 'gs://nbaya/risk_gradients/gwas/'
 
 def get_mt(phen, variant_set, seed=None, test_set=0.1):
     mt0 = hl.read_matrix_table(f'gs://nbaya/split/ukb31063.{variant_set}_variants.gwas_samples_repart.mt')
 
     print(f'\nReading UKB phenotype {phen_dict[phen]} (code: {phen})...')
+
     phen_tb0 = hl.import_table('gs://phenotype_31063/ukb31063.phesant_phenotypes.both_sexes.tsv.bgz',
                                missing='',impute=True,types={'"userId"': hl.tstr}).rename({ '"userId"': 's', '"'+phen+'"': 'phen'})
     phen_tb0 = phen_tb0.key_by('s')
@@ -80,10 +68,11 @@ def get_mt(phen, variant_set, seed=None, test_set=0.1):
     n = mt1.count_cols()
     n_cas = mt1.filter_cols(mt1.phen == 1).count_cols()
 
+    #Create training and testing matrix tables
     seed = seed if seed is not None else int(str(Env.next_seed())[:8])
     n_train = int(round(n*(1-test_set)))
     n_test = n-n_train
-    print('\n*****************')
+    print('*****************')
     print(f'Setting {test_set} of total population to be in the testing set')
     print(f'n_train = {n_train}\tn_test = {n_test}')
     print(f'seed = {seed}')
@@ -93,10 +82,15 @@ def get_mt(phen, variant_set, seed=None, test_set=0.1):
     randstate.shuffle(labels)
     mt2 = mt1.add_col_index('tmp_index').key_cols_by('tmp_index')
     mt3 = mt2.annotate_cols(set = hl.literal(labels)[hl.int32(mt2.tmp_index)])
-    mt3 = mt3.key_cols_by('s').drop('tmp_index')
+    # if not train_mt_done:
+    # mt3.filter_cols(mt3.label == 'train').write(train_mt_path,overwrite=True)
+    # if not test_mt_done:
+    # mt3.filter_cols(mt3.label == 'test').write(test_mt_path, overwrite=True)
+    # train_mt = hl.read_matrix_table(train_mt_path)
+    # test_mt = hl.read_matrix_table(test_mt_path)
     train_mt = mt3.filter_cols(mt3.set == 'train')
     test_mt = mt3.filter_cols(mt3.set == 'test')
-    
+
     n_cas_train = train_mt.filter_cols(train_mt.phen == 1).count_cols()
     n_cas_test = test_mt.filter_cols(test_mt.phen == 1).count_cols()
 
@@ -109,61 +103,133 @@ def get_mt(phen, variant_set, seed=None, test_set=0.1):
 
     return train_mt, n_train, n_cas_train, test_mt, n_test, n_cas_test
 
+def downsample(mt, frac, phen, for_cases=None, seed = None):
+    start = dt.datetime.now()
+    assert frac <= 1 and frac >= 0, "frac must be in [0,1]"
+    phen_name = phen._ir.name
+    n = mt.count_cols()
+    n_cas = mt.filter_cols(mt[phen_name]==1).count_cols()
+    if frac == 1:
+        return mt, n, n_cas
+    else:
+        seed = seed if seed is not None else int(str(Env.next_seed())[:8])
+        header = '\n************\n'
+        header += 'Downsampling '+('all' if for_cases is None else ('cases'*for_cases+'controls'*(for_cases==0)))+f' by frac = {frac}\n'
+        header += f'n: {n}\n'
+        header += f'n_cas: {n_cas}\nn_con: {n-n_cas}\nprevalence: {round(n_cas/n,6)}\n' if for_cases != None else ''
+        header += f'seed: {seed}\n'
+        header += '************'
+        print(header)
+        col_key = mt.col_key
+        randstate = np.random.RandomState(int(seed)) #seed random state for replicability
+        for_cases = bool(for_cases) if for_cases != None else None
+        filter_arg = (mt[phen_name] == (for_cases==0)) if for_cases != None else (hl.is_defined(mt[phen_name])==False)
+        mtA = mt.filter_cols(filter_arg) #keep all individuals in this mt
+        mtB = mt.filter_cols(filter_arg , keep=False) #downsample individuals in this mt
+        mtB = mtB.add_col_index('col_idx_tmpB')
+        mtB = mtB.key_cols_by('col_idx_tmpB')
+        nB = n_cas*for_cases + (n-n_cas)*(for_cases==0) if for_cases is not None else n
+        n_keep = int(nB*frac)
+        labels = ['A']*(n_keep)+['B']*(nB-n_keep)
+        randstate.shuffle(labels)
+        mtB = mtB.annotate_cols(label = hl.literal(labels)[hl.int32(mtB.col_idx_tmpB)])
+        mtB = mtB.filter_cols(mtB.label == 'A')
+        mtB = mtB.key_cols_by(*col_key)
+        mtB = mtB.drop('col_idx_tmpB','label')
+        mt1 = mtA.union_cols(mtB)
+        n_new = mt1.count_cols()
+        n_cas_new = mt1.filter_cols(mt1[phen_name]==1).count_cols()
+        elapsed = dt.datetime.now()-start
+        print('\n************')
+        print('Finished downsampling '+('all' if for_cases is None else ('cases'*for_cases+'controls'*(for_cases==0)))+f' by frac = {frac}')
+        print(f'n: {n} -> {n_new} ({round(100*n_new/n,3)}% of original)')
+        if n_cas != 0 and n_new != 0 :
+            print(f'n_cas: {n_cas} -> {n_cas_new} ({round(100*n_cas_new/n_cas,3)}% of original)')
+            print(f'n_con: {n-n_cas} -> {n_new-n_cas_new} ({round(100*(n_new-n_cas_new)/(n-n_cas),3)}% of original)')
+            print(f'prevalence: {round(n_cas/n,6)} -> {round(n_cas_new/n_new,6)} ({round(100*(n_cas_new/n_new)/(n_cas/n),6)}% of original)')
+        print(f'Time for downsampling: '+str(round(elapsed.seconds/60, 2))+' minutes')
+        print('************')
+        return mt1, n_new, n_cas
 
 
-if __name__=="__main__":
-    
-    for i, phen in enumerate(phen_ls):
-        _, n, n_cas, mt1, _, _= get_mt(phen, 'hm3', seed=seed) #get n and n_cas from training set, and get testing set matrix table
+if __name__ == "__main__":
+    header =  '\n*************\n'
+    header += f'Phenotypes to downsample: {[phen_dict[phen]+f" (code: {phen})" for phen in phen_ls]}\n'
+    header += f'Downsampling fractions for all: {frac_all_ls}\n' if frac_all_ls != None else ''
+    header += f'Downsampling fractions for cases: {frac_cas_ls}\n' if frac_cas_ls != None else ''
+    header += f'Downsampling fractions for controls: {frac_con_ls}\n' if frac_con_ls != None else ''
+    header += f'Random seed: {seed}\n'
+    header += '*************'
+    print(header)
+
+    for phen in phen_ls:
+        print('\n*************')
+        print(f'Starting phenotype: {phen_dict[phen]} (code: {phen})')
+        print('Time: {:%H:%M:%S (%Y-%b-%d)}'.format(dt.datetime.now()))
+        print('*************')
+        mt, n, n_cas, _, _, _ = get_mt(phen, variant_set, seed=seed, )
+
+        start_phen = dt.datetime.now()
+
         for frac_all in frac_all_ls:
-            n_new0 = int(frac_all*n)
+            mt1, _, _ = downsample(mt=mt,frac=frac_all,phen=mt.phen,for_cases=None,seed=seed)
             for frac_cas in frac_cas_ls:
-                n_cas_new = int(frac_cas*n_cas)
+                mt2, _, _ = downsample(mt=mt1,frac=frac_cas,phen=mt1.phen,for_cases=1,seed=seed)
+                # if len(frac_cas_ls)>1 or frac_cas_ls != [1]:
+                #     print('\ncheckpointing matrix table')
+                #     mt2 = mt2.checkpoint(f'{gwas_wd}tmp.mt')
                 for frac_con in frac_con_ls:
-                    n_new = int(frac_con*(n_new0-n_cas_new)+n_cas_new)
+                    start_iter = dt.datetime.now()
+                    mt3, n_new, n_cas_new = downsample(mt=mt2,frac=frac_con,phen=mt2.phen,for_cases=0,seed=seed)
+
+                    gt0 = hl.read_matrix_table('gs://phenotype_31063/hail/imputed/ukb31063.GT.autosomes.mt/')
+                    mt3 = mt3.annotate_entries(GT = gt0[(mt3.locus,mt3.alleles),mt3.s].GT)
+                    mt3 = mt3.annotate_rows(maf = hl.agg.stats(mt3.dosage).mean/2) # annotate with maf
                     if frac_con == 1 and frac_cas ==1:
-                        suffix = f'.{phen}.n_{n_new}of{n}.seed_{seed}'
+                        id_path = f'{gwas_wd}iid.{phen}.n_{n_new}of{n}.seed_{seed}.tsv.bgz'
                     else:
-                        suffix = f'.{phen}.n_{n_new}of{n}.n_cas_{n_cas_new}of{n_cas}.seed_{seed}'
-                    
-                    print('importing table...')
-                    gwas = hl.import_table(f'{gwas_wd}ss{suffix}.tsv.bgz',force_bgz=True,impute=True)
-                    gwas = gwas.key_by('SNP')
-                    
-                    print('annotating with betas...')
-                    mt2 = mt1.annotate_rows(beta = gwas[mt1.rsid].eff)
-                    
-                    print('calculating PGS...')
-                    print(f'frac_all: {frac_all}\tfrac_cas: {frac_cas}\tfrac_con: {frac_con}')
-                    print('Time: {:%H:%M:%S (%Y-%b-%d)}'.format(dt.datetime.now()))
-                    start_pgs = dt.datetime.now()
-                    mt3 = mt2.annotate_cols(pgs = hl.agg.sum(mt2.dosage*mt2.beta))
-                    mt3.cols().select('phen','pgs').export(f'{wd}pgs{suffix}.tsv.bgz')
-                    elapsed_pgs = dt.datetime.now()-start_pgs
-                    print('\nFinished calculating PGS')
-                    print(f'Time for calculating PGS: {round(elapsed_pgs.seconds/60, 2)} minutes')
-    
-                    print('calculating R^2 between PGS and phenotype...')
-                    print('Time: {:%H:%M:%S (%Y-%b-%d)}'.format(dt.datetime.now()))
-                    start_r2 = dt.datetime.now()
-                    if not os.path.isdir(local_wd):
-                        os.mkdir(local_wd)
-                    subprocess.call(['gsutil','cp',f'{wd}pgs{suffix}.tsv.bgz',local_wd])
-                    subprocess.call(['gsutil','cp',f'{gwas_wd}iid{suffix}.tsv.bgz',local_wd])
-                    df = pd.read_csv(f'{local_wd}pgs{suffix}.tsv.bgz',delimiter='\t',compression='gzip')
-                    r_all, pval_all = stats.pearsonr(df.pgs, df.phen)
-                    iid = pd.read_csv(f'{local_wd}iid{suffix}.tsv.bgz',delimiter='\t',compression='gzip')
-                    df1 = df[df.s.isin(iid.iid.tolist())]
-                    r_sub, pval_sub = stats.pearsonr(df1.pgs,df1.phen)
-                    print('\n****************************')
-                    print(f'PGS-phenotype correlation for all {n} individuals')
-                    print(f'r = {r_all}, pval = {pval_all}')
-                    print(f'PGS-phenotype correlation for all {n_new} individuals')
-                    print(f'r = {r_sub}, pval = {pval_sub}')
-                    print('****************************')
-                    array = [[r_all, pval_all],[r_sub, pval_sub]]
-                    np.savetxt(f'{local_wd}corr{suffix}.txt',array,delimiter='\t')
-                    subprocess.call(['gsutil','cp',f'{local_wd}corr{suffix}.txt',wd])
-                    elapsed_r2 = dt.datetime.now()-start_r2
-                    print('\nFinished calculating R^2')
-                    print(f'Time for calculating R^2: {round(elapsed_r2.seconds/60, 2)} minutes')
+                        id_path = f'{gwas_wd}iid.{phen}.n_{n_new}of{n}.n_cas_{n_cas_new}of{n_cas}.seed_{seed}.tsv.bgz'
+                    cols = mt3.cols().key_by().select('s')
+                    cols = cols.annotate(iid=cols.s).annotate(s=1).rename({'s':'fid'}).export(id_path)
+
+                    cov_list = [ mt3['isFemale'], mt3['age'], mt3['age_squared'], mt3['age_isFemale'],
+                        mt3['age_squared_isFemale'] ]+ [mt3['PC{:}'.format(i)] for i in range(1, 21)]
+
+                    ht = hl.linear_regression_rows(
+                            y=mt3.phen,
+                            x=mt3.dosage,
+                            covariates=[1]+cov_list,
+                            pass_through = ['rsid','maf'])
+
+                    ht = ht.rename({'rsid':'SNP'}).key_by('SNP')
+
+                    ss_template = hl.read_table('gs://nbaya/rg_sex/hm3.sumstats_template.ht/')
+                    ss_template  = ss_template.key_by('SNP')
+
+                    ss = ss_template.annotate(N = n_new)
+                    locus = hl.str(ht[ss.SNP].locus).split(':')
+                    ss = ss.annotate(ch = locus[0],
+                                     pos = locus[1].split('_')[0],
+                                     pval = ht[ss.SNP]['p_value'],
+                                     maf = ht[ss.SNP]['maf'],
+                                     eff = ht[ss.SNP]['beta'])
+
+                    if frac_con == 1 and frac_cas ==1:
+                        path = f'{gwas_wd}ss.{phen}.n_{n_new}of{n}.seed_{seed}.tsv.bgz'
+                    else:
+                        path = f'{gwas_wd}ss.{phen}.n_{n_new}of{n}.n_cas_{n_cas_new}of{n_cas}.seed_{seed}.tsv.bgz'
+                    ss.export(path)
+                    elapsed_iter = dt.datetime.now()-start_iter
+                    print('\n*************')
+                    print(f'Finished GWAS for downsampled phenotype: {phen_dict[phen]} (code: {phen})')
+                    print(f'frac_all = {frac_all}, frac_cas = {frac_cas}, frac_con = {frac_con}')
+                    print(f'Time for downsampled GWAS: '+str(round(elapsed_iter.seconds/60, 2))+' minutes')
+                    print('*************')
+
+        elapsed_phen = dt.datetime.now()-start_phen
+        print('\n*************')
+        print(f'Finished phenotype: {phen_dict[phen]} (code: {phen})')
+        print(f'Number of downsampling fraction combinations: {len(frac_all_ls)*len(frac_con_ls)*len(frac_cas_ls)}')
+        print('Time: {:%H:%M:%S (%Y-%b-%d)}'.format(dt.datetime.now()))
+        print(f'Time for phenotype: '+str(round(elapsed_phen.seconds/60, 2))+' minutes')
+        print('*************')
