02e226848f164f615ead6a07ec36c1ea1814c4fe
Nik Baya <40802812+nikbaya@users.noreply.github.com>
2019-06-14 09:28:22 -0400

    updated to reconcile pull/push

--- calc_scores.py
+++ calc_scores.py
@@ -16,6 +16,8 @@ import subprocess
 import pandas as pd
 import datetime as dt
 import os
+from hail.utils.java import Env
+
 
 
 parser = argparse.ArgumentParser(add_help=False)
@@ -52,46 +54,67 @@ phen_dict = {
     '21001':'bmi',
 }
 
-def get_mt(phen, variant_set):
+def get_mt(phen, variant_set, seed=None, test_set=0.1):
     mt0 = hl.read_matrix_table(f'gs://nbaya/split/ukb31063.{variant_set}_variants.gwas_samples_repart.mt')
-    
+
     print(f'\nReading UKB phenotype {phen_dict[phen]} (code: {phen})...')
-    #        mt0 = hl.read_matrix_table('gs://nbaya/split/ukb31063.hm3_variants.gwas_samples_v2.mt') #old version
-        
     phen_tb0 = hl.import_table('gs://phenotype_31063/ukb31063.phesant_phenotypes.both_sexes.tsv.bgz',
                                missing='',impute=True,types={'"userId"': hl.tstr}).rename({ '"userId"': 's', '"'+phen+'"': 'phen'})
     phen_tb0 = phen_tb0.key_by('s')
-    phen_tb = phen_tb0.select(phen_tb0['phen'])    
-    
+    phen_tb = phen_tb0.select(phen_tb0['phen'])
+
     mt1 = mt0.annotate_cols(phen_str = hl.str(phen_tb[mt0.s]['phen']).replace('\"',''))
     mt1 = mt1.filter_cols(mt1.phen_str == '',keep=False)
-    
+
     if phen_tb.phen.dtype == hl.dtype('bool'):
         mt1 = mt1.annotate_cols(phen = hl.bool(mt1.phen_str)).drop('phen_str')
     else:
-        mt1 = mt1.annotate_cols(phen = hl.float64(mt1.phen_str)).drop('phen_str')            
-    
+        mt1 = mt1.annotate_cols(phen = hl.float64(mt1.phen_str)).drop('phen_str')
+
     #Remove withdrawn samples
     withdrawn = hl.import_table('gs://nbaya/w31063_20181016.csv',missing='',no_header=True)
     withdrawn_set = set(withdrawn.f0.take(withdrawn.count()))
-    mt1 = mt1.filter_cols(hl.literal(withdrawn_set).contains(mt1['s']),keep=False) 
+    mt1 = mt1.filter_cols(hl.literal(withdrawn_set).contains(mt1['s']),keep=False)
     mt1 = mt1.key_cols_by('s')
-    
+
     n = mt1.count_cols()
     n_cas = mt1.filter_cols(mt1.phen == 1).count_cols()
-    
-    variants = hl.import_table(wd+'ukb_imp_v3_pruned.bim',delimiter='\t',no_header=True,impute=True)
-    variants = variants.rename({'f0':'chr','f1':'rsid','f3':'pos'}).key_by('rsid')
-    
-    mt2 = mt1.key_rows_by('rsid')
-    mt2 = mt2.filter_rows(hl.is_defined(variants[mt2.rsid])) #filter to variants in variants table
 
-    return mt2, n, n_cas
+    seed = seed if seed is not None else int(str(Env.next_seed())[:8])
+    n_train = int(round(n*(1-test_set)))
+    n_test = n-n_train
+    print('\n*****************')
+    print(f'Setting {test_set} of total population to be in the testing set')
+    print(f'n_train = {n_train}\tn_test = {n_test}')
+    print(f'seed = {seed}')
+    print('*****************')
+    randstate = np.random.RandomState(int(seed)) #seed random state for replicability
+    labels = ['train']*n_train+['test']*n_test
+    randstate.shuffle(labels)
+    mt2 = mt1.add_col_index('tmp_index').key_cols_by('tmp_index')
+    mt3 = mt2.annotate_cols(set = hl.literal(labels)[hl.int32(mt2.tmp_index)])
+    mt3 = mt3.key_cols_by('s').drop('tmp_index')
+    train_mt = mt3.filter_cols(mt3.set == 'train')
+    test_mt = mt3.filter_cols(mt3.set == 'test')
+    
+    n_cas_train = train_mt.filter_cols(train_mt.phen == 1).count_cols()
+    n_cas_test = test_mt.filter_cols(test_mt.phen == 1).count_cols()
+
+    print('*****************')
+    print(f'Original prevalence of {phen_dict[phen]} (code: {phen}): {round(n_cas/n,6)}')
+    print(f'Prevalence in training dataset: {round(n_cas_train/n_train,6)}')
+    print(f'Prevalence in testing dataset: {round(n_cas_test/n_test,6)}')
+    print(f'If trait is not case/control, these will probably all be 0.')
+    print('*****************')
+
+    return train_mt, n_train, n_cas_train, test_mt, n_test, n_cas_test
+
+
 
 if __name__=="__main__":
     
     for i, phen in enumerate(phen_ls):
-        mt1, n, n_cas = get_mt(phen, 'hm3')
+        _, n, n_cas, mt1, _, _= get_mt(phen, 'hm3', seed=seed) #get n and n_cas from training set, and get testing set matrix table
         for frac_all in frac_all_ls:
             n_new0 = int(frac_all*n)
             for frac_cas in frac_cas_ls:
@@ -111,7 +134,7 @@ if __name__=="__main__":
                     mt2 = mt1.annotate_rows(beta = gwas[mt1.rsid].eff)
                     
                     print('calculating PGS...')
-                    print(f'frac_all: {frac_all}\tfrac_all: {frac_cas}\tfrac_all: {frac_con}')
+                    print(f'frac_all: {frac_all}\tfrac_cas: {frac_cas}\tfrac_con: {frac_con}')
                     print('Time: {:%H:%M:%S (%Y-%b-%d)}'.format(dt.datetime.now()))
                     start_pgs = dt.datetime.now()
                     mt3 = mt2.annotate_cols(pgs = hl.agg.sum(mt2.dosage*mt2.beta))
