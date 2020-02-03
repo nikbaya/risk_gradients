35a34d72b6fcd8e8fbb837353ef3795338c8d075
nikbaya <nbaya@broadinstitute.org>
2020-02-03 12:17:51 -0500

    a (seemingly) working version of PRS-CS across chroms

--- python/msprime_prs.py
+++ python/msprime_prs.py
@@ -16,6 +16,7 @@ To setup VM:
 import argparse
 from datetime import datetime as dt
 import numpy as np
+from scipy import linalg, random, stats
 import math
 import msprime
 
@@ -148,12 +149,12 @@ def sim_ts(args):
     # load recombination maps
     if args.rec_map_chr:
         rec_map_list = []
-        for chr in range(args.n_chr):
-            fname = args.rec_map_chr.replace('@',str(chr+1))
+        for chr_idx in range(args.n_chr):
+            fname = args.rec_map_chr.replace('@',str(chr_idx+1))
             rec_map_list.append(msprime.RecombinationMap.read_hapmap(fname))
         args.rec, args.m_per_chr = None, None
     else:
-        rec_map_list = [None]*args.n_chr
+        rec_map_list = [None for x in range(args.n_chr)]
 
     # simulate with out-of-Africa model
     n_total = args.n + args.n_ref
@@ -166,12 +167,12 @@ def sim_ts(args):
                                     demographic_events=demographic_events)
     dp.print_history()
 
-    for chr in range(args.n_chr):
+    for chr_idx in range(args.n_chr):
         ts_list_all.append(msprime.simulate(sample_size=None, #set to None because sample_size info is stored in pop_configs
                                             population_configurations=pop_configs,
                                             migration_matrix=migration_mat,
                                             demographic_events=demographic_events,
-                                            recombination_map=rec_map_list[chr],
+                                            recombination_map=rec_map_list[chr_idx],
                                             length=args.m_per_chr, Ne=Ne,
                                             recombination_rate=args.rec,
                                             mutation_rate=args.mut))
@@ -179,16 +180,16 @@ def sim_ts(args):
         # get mutations > MAF
 #        ts_list_all[chr] = get_common_mutations_ts(args, ts_list_all[chr]) # comment out to run later phenotype simulation with causal SNPs not genotyped
 
-        m[chr] = int(ts_list_all[chr].get_num_mutations())
-        m_start[chr] = m_total
-        m_total += m[chr]
-        print(f'Number of mutations in chr {chr+1}: {m[chr]}')
+        m[chr_idx] = int(ts_list_all[chr_idx].get_num_mutations())
+        m_start[chr_idx] = m_total
+        m_total += m[chr_idx]
+        print(f'Number of mutations in chr {chr_idx+1}: {m[chr_idx]}')
         print(f'Running total of sites : {m_total}')
 
-        ts_list_geno_all.append(ts_list_all[chr])
-        genotyped_list_index.append(np.ones(ts_list_all[chr].num_mutations, dtype=bool))
-        m_geno[chr] = m[chr]
-        m_geno_start[chr] = m_start[chr]
+        ts_list_geno_all.append(ts_list_all[chr_idx])
+        genotyped_list_index.append(np.ones(ts_list_all[chr_idx].num_mutations, dtype=bool))
+        m_geno[chr_idx] = m[chr_idx]
+        m_geno_start[chr_idx] = m_start[chr_idx]
         m_geno_total = m_total
 #        print(f'Number of sites genotyped in the chr {chr+1}: {m_geno[chr]}')
 #        print(f'Running total of sites genotyped: {m_geno_total}')
@@ -207,15 +208,15 @@ def _update_vars(args, ts_list):
     genotyped_list_index = []
     m_total = 0
     m_geno_total = 0
-    for chr in range(args.n_chr):
-        m[chr] = int(ts_list[chr].get_num_mutations())
-        m_start[chr] = m_total
-        m_total += m[chr]
+    for chr_idx in range(args.n_chr):
+        m[chr_idx] = int(ts_list[chr_idx].get_num_mutations())
+        m_start[chr_idx] = m_total
+        m_total += m[chr_idx]
 
-        ts_list_geno.append(ts_list[chr])
-        genotyped_list_index.append(np.ones(ts_list[chr].num_mutations, dtype=bool))
-        m_geno[chr] = m[chr]
-        m_geno_start[chr] = m_start[chr]
+        ts_list_geno.append(ts_list[chr_idx])
+        genotyped_list_index.append(np.ones(ts_list[chr_idx].num_mutations, dtype=bool))
+        m_geno[chr_idx] = m[chr_idx]
+        m_geno_start[chr_idx] = m_start[chr_idx]
         m_geno_total = m_total
     return ts_list_geno, genotyped_list_index, m_total, m_geno_total
     
@@ -232,17 +233,17 @@ def split(args, ts_list_all, ts_list_geno_all):
            m_geno_start, m_geno_total, genotyped_list_index
 
 def nextSNP_add(variant, index=None):
-    	if index is None:
-    		var_tmp = np.array(variant.genotypes[0::2].astype(int)) + np.array(variant.genotypes[1::2].astype(int))
-    	else:
-    		var_tmp = np.array(variant.genotypes[0::2][index].astype(int)) + np.array(variant.genotypes[1::2][index].astype(int))
+	if index is None:
+		var_tmp = np.array(variant.genotypes[0::2].astype(int)) + np.array(variant.genotypes[1::2].astype(int))
+	else:
+		var_tmp = np.array(variant.genotypes[0::2][index].astype(int)) + np.array(variant.genotypes[1::2][index].astype(int))
 
-    	# Additive term.
-    	mean_X = np.mean(var_tmp)
+	# Additive term.
+	mean_X = np.mean(var_tmp)
 #    	p = mean_X / 2
-    	# Evaluate the mean and then sd to normalise.
-    	X_A = (var_tmp - mean_X) / np.std(var_tmp)
-    	return X_A
+	# Evaluate the mean and then sd to normalise.
+	X_A = (var_tmp - mean_X) / np.std(var_tmp)
+	return X_A
 
 
 def sim_phen(args, n_pops, ts_list, m_total):
@@ -280,11 +281,11 @@ def sim_phen(args, n_pops, ts_list, m_total):
     beta_A_list = [] # list of np arrays (one for each chromosome) containing true effect sizes
     ts_pheno_A_list = [] # list of tree sequences on which phenotypes are calculated (possibly ignoring invariant SNPs?)
     
-    for chr in range(args.n_chr):
-        m_chr = int(ts_list[chr].get_num_mutations())
-        print(f'Picking causal variants and determining effect sizes in chromosome {chr+1}')
+    for chr_idx in range(args.n_chr):
+        m_chr = int(ts_list[chr_idx].get_num_mutations())
+        print(f'Picking causal variants and determining effect sizes in chromosome {chr_idx+1}')
         print(f'p-causal is {args.p_causal}')
-        ts_pheno_A, m_causal_A, causal_A_index = set_mutations_in_tree(ts_list[chr], args.p_causal)
+        ts_pheno_A, m_causal_A, causal_A_index = set_mutations_in_tree(ts_list[chr_idx], args.p_causal)
         print(f'Picked {m_causal_A} additive causal variants out of {m_chr}')
         beta_A = np.random.normal(loc=0, scale=np.sqrt(args.h2_A / (m_total * args.p_causal)), size=m_causal_A)
         beta_A_list.append(beta_A)
@@ -301,9 +302,9 @@ def joint_maf_filter(ts_list1, ts_list2, maf=0.05):
         r'''
         Filter to SNPs with MAF>`maf` in both `ts_list1` and `ts_list2`
         '''
-        for chr in range(args.n_chr):
-            ts1 = ts_list1[chr]
-            ts2 = ts_list2[chr]
+        for chr_idx in range(args.n_chr):
+            ts1 = ts_list1[chr_idx]
+            ts2 = ts_list2[chr_idx]
 
             n_haps1 = ts1.get_sample_size()
             n_haps2 = ts2.get_sample_size()
@@ -353,47 +354,255 @@ def joint_maf_filter(ts_list1, ts_list2, maf=0.05):
                     site=common_site_id2,
                     node=site2.mutations[0].node,
                     derived_state=site2.mutations[0].derived_state)
-            ts_list1[chr] = tables1.tree_sequence()
-            ts_list2[chr] = tables2.tree_sequence()
+            ts_list1[chr_idx] = tables1.tree_sequence()
+            ts_list2[chr_idx] = tables2.tree_sequence()
 
         return ts_list1, ts_list2
     
-def run_gwas(args, y, ts_list_geno, m_geno_total):
+def run_gwas(args, y, ts_list_geno):
     r'''
     Get GWAS beta-hats
     '''
-    betahat_A_list = [None]*args.n_chr # list of np arrays (one for each chromosome) holding GWAS beta-hats
+    betahat_A_list = [None for i in range(args.n_chr)] # list of np arrays (one for each chromosome) holding GWAS beta-hats
+    maf_A_list = [None for i in range(args.n_chr)] # list of np arrays (one for each chromosome) holding GWAS MAF
+#    pval_A_list = [None]*args.n_chr # list of np arrays (one for each chromosome) holding GWAS pvals
 
     n = int(ts_list_geno[0].get_sample_size()/2 ) # assume that sample size is same across chromosomes
-    for chr in range(args.n_chr):
-        intercept = np.ones(shape=(1,n)) # vector of intercepts for least sq linear regression
-        betahat_A = np.empty(shape=m_geno_total)
-        print(f'Determining beta-hats in chromosome {chr+1}')
-        for k, variant in enumerate(ts_list_geno[chr].variants()):
+    for chr_idx in range(args.n_chr):
+        m_gwas = ts_list_geno[chr_idx].get_num_mutations()
+        betahat_A = np.empty(shape=m_gwas)
+        maf_A = np.empty(shape=m_gwas)
+#        pval_A = np.empty(shape=m_geno_total)
+        print(f'Determining beta-hats in chromosome {chr_idx+1}')
+        for k, variant in enumerate(ts_list_geno[chr_idx].variants()):
                 X_A = nextSNP_add(variant, index=None)
-                X_A_w_int = np.vstack((X_A.reshape(1, n), intercept)).T
-                coef, _, _, _ = np.linalg.lstsq(X_A_w_int, y.reshape(n,),rcond=None)
-                betahat_A[k] = coef[0] # take only the beta for the genotypes, not the intercept
-        betahat_A_list[chr] = betahat_A
+                betahat, _, _, pval, _ = stats.linregress(x=X_A, y=y.reshape(n,))
+                betahat_A[k] = betahat
+                af = variant.genotypes.astype(int).mean()
+                maf = min(af, 1-af)
+                maf_A[k] = maf
+#                pval_A[k] = pval
+        betahat_A_list[chr_idx] = betahat_A
+        maf_A_list[chr_idx] = maf_A
+#        pval_A_list[chr_idx] = pval_A
 
-    return betahat_A_list
+    return betahat_A_list, maf_A_list
 
 def calc_ld(args, ts_list_ref):
     r'''
     Calculate LD for reference panel
     '''
     ld_list = []
-    for chr in range(args.n_chr):
-        X = ts_list_ref[chr].genotype_matrix()
+    for chr_idx in range(args.n_chr):
+        X = ts_list_ref[chr_idx].genotype_matrix()
         ld = np.corrcoef(X)
-        ld_list.append(ld)
+        ld_list.append([ld])
     return ld_list
 
-def prs_cs(args, betahat_A_list, ld_list):
+def prs_cs(args, betahat_A_list, maf_A_list, ld_list):
     r'''
     Use PRS-CS to calculate adjusted beta-hats
     '''
-    pass
+    def _psi(x, alpha, lam):
+        f = -alpha*(math.cosh(x)-1)-lam*(math.exp(x)-x-1)
+        return f
+    
+    def _dpsi(x, alpha, lam):
+        f = -alpha*math.sinh(x)-lam*(math.exp(x)-1)
+        return f
+    
+    def _g(x, sd, td, f1, f2):
+        if (x >= -sd) and (x <= td):
+            f = 1
+        elif x > td:
+            f = f1
+        elif x < -sd:
+            f = f2
+    
+        return f
+    
+    def gigrnd(p, a, b):
+        # setup -- sample from the two-parameter version gig(lam,omega)
+        p = float(p); a = float(a); b = float(b)
+        lam = p
+        omega = math.sqrt(a*b)
+    
+        if lam < 0:
+            lam = -lam
+            swap = True
+        else:
+            swap = False
+    
+        alpha = math.sqrt(math.pow(omega,2)+math.pow(lam,2))-lam
+        
+        # find t
+        x = -_psi(1, alpha, lam)
+        if (x >= 1/2) and (x <= 2):
+            t = 1
+        elif x > 2:
+            t = math.sqrt(2/(alpha+lam))
+        elif x < 1/2:
+            t = math.log(4/(alpha+2*lam))
+    
+        # find s
+        x = -_psi(-1, alpha, lam)
+        if (x >= 1/2) and (x <= 2):
+            s = 1
+        elif x > 2:
+            s = math.sqrt(4/(alpha*math.cosh(1)+lam))
+        elif x < 1/2:
+            s = min(1/lam, math.log(1+1/alpha+math.sqrt(1/math.pow(alpha,2)+2/alpha)))
+    
+        # find auxiliary parameters
+        eta = -_psi(t, alpha, lam)
+        zeta = -_dpsi(t, alpha, lam)
+        theta = -_psi(-s, alpha, lam)
+        xi = _dpsi(-s, alpha, lam)
+    
+        p = 1/xi
+        r = 1/zeta
+    
+        td = t-r*eta
+        sd = s-p*theta
+        q = td+sd
+    
+        # random variate generation
+        while True:
+            U = random.random()
+            V = random.random()
+            W = random.random()
+            if U < q/(p+q+r):
+                rnd = -sd+q*V
+            elif U < (q+r)/(p+q+r):
+                rnd = td-r*math.log(V)
+            else:
+                rnd = -sd+p*math.log(V)
+    
+            f1 = math.exp(-eta-zeta*(rnd-t))
+            f2 = math.exp(-theta+xi*(rnd+s))
+            if W*_g(rnd, sd, td, f1, f2) <= math.exp(_psi(rnd, alpha, lam)):
+                break
+    
+        # transform back to the three-parameter version gig(p,a,b)
+        rnd = math.exp(rnd)*(lam/omega+math.sqrt(1+math.pow(lam,2)/math.pow(omega,2)))
+        if swap:
+            rnd = 1/rnd
+    
+        rnd = rnd/math.sqrt(a/b)
+        return rnd
+
+    def mcmc(a, b, phi, sst_dict, n, ld_blk, blk_size, n_iter, n_burnin, thin, 
+             chrom, beta_std, seed):
+        print('... MCMC ...')
+    
+        # seed
+        if seed != None:
+            random.seed(seed)
+    
+        # derived stats
+        beta_mrg = np.array(sst_dict['BETA']).T
+        beta_mrg = np.expand_dims(beta_mrg, axis=1)
+        maf = np.array(sst_dict['MAF']).T
+        n_pst = (n_iter-n_burnin)/thin
+#        p = len(sst_dict['SNP'])
+        p = len(sst_dict['BETA'])
+        n_blk = len(ld_blk)
+    
+        # initialization
+        beta = np.zeros((p,1))
+        psi = np.ones((p,1))
+        sigma = 1.0
+        if phi == None:
+            phi = 1.0; phi_updt = True
+        else:
+            phi_updt = False
+    
+        beta_est = np.zeros((p,1))
+        psi_est = np.zeros((p,1))
+        sigma_est = 0.0
+        phi_est = 0.0
+    
+        # MCMC
+        for itr in range(1,n_iter+1):
+            if itr % 100 == 0:
+                print('--- iter-' + str(itr) + ' ---')
+    
+            mm = 0; quad = 0.0
+            for kk in range(n_blk):
+                if blk_size[kk] == 0:
+                    continue
+                else:
+                    idx_blk = range(mm,mm+blk_size[kk])
+                    dinvt = ld_blk[kk]+np.diag(1.0/psi[idx_blk].T[0])
+                    dinvt_chol = linalg.cholesky(dinvt)
+                    beta_tmp = linalg.solve_triangular(dinvt_chol, beta_mrg[idx_blk], trans='T') + np.sqrt(sigma/n)*random.randn(len(idx_blk),1)
+                    beta[idx_blk] = linalg.solve_triangular(dinvt_chol, beta_tmp, trans='N')
+                    quad += np.dot(np.dot(beta[idx_blk].T, dinvt), beta[idx_blk])
+                    mm += blk_size[kk]
+    
+            err = max(n/2.0*(1.0-2.0*sum(beta*beta_mrg)+quad), n/2.0*sum(beta**2/psi))
+            sigma = 1.0/random.gamma((n+p)/2.0, 1.0/err)
+    
+            delta = random.gamma(a+b, 1.0/(psi+phi))
+    
+            for jj in range(p):
+                psi[jj] = gigrnd(a-0.5, 2.0*delta[jj], n*beta[jj]**2/sigma)
+            psi[psi>1] = 1.0
+    
+            if phi_updt == True:
+                w = random.gamma(1.0, 1.0/(phi+1.0))
+                phi = random.gamma(p*b+0.5, 1.0/(sum(delta)+w))
+    
+            # posterior
+            if (itr>n_burnin) and (itr % thin == 0):
+                beta_est = beta_est + beta/n_pst
+                psi_est = psi_est + psi/n_pst
+                sigma_est = sigma_est + sigma/n_pst
+                phi_est = phi_est + phi/n_pst
+    
+        # convert standardized beta to per-allele beta
+        if beta_std == 'False':
+            beta_est /= np.sqrt(2.0*maf*(1.0-maf))
+    
+#        # write posterior effect sizes
+#        if phi_updt == True:
+#            eff_file = out_dir + '_pst_eff_a%d_b%.1f_phiauto_chr%d.txt' % (a, b, chrom)
+#        else:
+#            eff_file = out_dir + '_pst_eff_a%d_b%.1f_phi%1.0e_chr%d.txt' % (a, b, phi, chrom)
+#    
+#        with open(eff_file, 'w') as ff:
+#            for snp, bp, a1, a2, beta in zip(sst_dict['SNP'], sst_dict['BP'], sst_dict['A1'], sst_dict['A2'], beta_est):
+#                ff.write('%d\t%s\t%d\t%s\t%s\t%.6e\n' % (chrom, snp, bp, a1, a2, beta))
+    
+        # print estimated phi
+        if phi_updt == True:
+            print('... Estimated global shrinkage parameter: %1.2e ...' % phi_est )
+    
+        print('... Done ...')    
+        return beta_est
+    
+#    (a, b, phi, sst_dict, n, ld_blk, blk_size, n_iter, n_burnin, thin, 
+#             chrom, out_dir, beta_std, seed)
+    a = 1; b = 0.5
+    phi = None
+    n = args.n
+    n_iter = 1000
+    n_burnin = 500
+    thin = 5
+    beta_std = True
+    seed = None
+    
+    sst_dict_list = [{'BETA':betahat_A_list[chr_idx], 'MAF':maf_A_list[chr_idx]}
+                     for chr_idx in range(args.n_chr)]
+    
+    beta_est_list = []
+    for chr_idx in range(args.n_chr):
+        sst_dict = sst_dict_list[chr_idx]
+        ld_blk = ld_list[chr_idx]
+        blk_size = [len(blk) for blk in ld_blk]
+        beta_est_list.append(mcmc(a, b, phi, sst_dict, n, ld_blk, blk_size, 
+                                  n_iter, n_burnin, thin, chr_idx+1, beta_std, seed))
+    return beta_est_list
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -422,25 +631,24 @@ if __name__ == '__main__':
     print(f'joint maf filter time: {round((dt.now()-start_joint_maf).seconds/60, 2)} min')
     
     geno_index_list = [] # list of lists
-    for chr in range(args.n_chr):
-        ts_pheno_A = ts_pheno_A_list[chr]
-        ts_geno = ts_list_geno[chr]
+    for chr_idx in range(args.n_chr):
+        ts_pheno_A = ts_pheno_A_list[chr_idx]
+        ts_geno = ts_list_geno[chr_idx]
         sites_all = [site.position for tree in ts_pheno_A.trees() for site in tree.sites()]
         sites_geno = [site.position for tree in ts_geno.trees() for site in tree.sites()]
         geno_index = [k for k, position in enumerate(sites_all) if position in sites_geno]
         geno_index = np.asarray(geno_index)
-        print(geno_index)
         geno_index_list.append(geno_index)
         
-    # run GWAS
+    # run GWAS (and calculate MAF along the way)
     start_run_gwas = dt.now()
-    betahat_A_list = run_gwas(args, y, ts_list_geno, m_geno_total)
+    betahat_A_list, maf_A_list = run_gwas(args, y, ts_list_geno)
     print(f'run gwas time: {round((dt.now()-start_run_gwas).seconds/60, 2)} min')
 
-    for chr in range(args.n_chr):
-        beta_A_geno = beta_A_list[chr][geno_index_list[chr]]
+    for chr_idx in range(args.n_chr):
+        beta_A_geno = beta_A_list[chr_idx][geno_index_list[chr_idx]]
         print(len(beta_A_geno))
-        r = np.corrcoef(np.vstack((beta_A_geno, betahat_A_list[chr])))[0,1]
+        r = np.corrcoef(np.vstack((beta_A_geno, betahat_A_list[chr_idx])))[0,1]
         print(f'correlation between betas: {r}') #subset to variants that were used in the GWAS
 
     # calculate LD matrix
@@ -450,5 +658,10 @@ if __name__ == '__main__':
     
     # run PRS-CS
     start_prs_cs = dt.now()
-    betahat_prscs = prs_cs(args, betahat_A_list, ld_list, )
+    beta_est_list = prs_cs(args, betahat_A_list, maf_A_list, ld_list)
+    print(f'prs-cs time: {round((dt.now()-start_prs_cs).seconds/60, 2)} min')
+    
+    for chr_idx in range(args.n_chr):
+        
+    
 #    ref_dict_ls, vld_dict_ls, sst_dict_ls, ld_blk_ls, blk_size_ls = sim_ts(args)
